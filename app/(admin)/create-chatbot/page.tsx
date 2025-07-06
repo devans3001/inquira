d51e70bb9f94d01eddbd74c2883fd3e7b0ebc9da
Evand Diegbe <diegbee0@gmail.com>
2025-07-06 18:44:34 +0100

    currently working on neon db

--- app/(admin)/create-chatbot/page.tsx
+++ app/(admin)/create-chatbot/page.tsx
@@ -1,12 +1,34 @@
 
 
+import Avatar from '@/components/avatar'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 
 function CreateChatbot() {
 
-   // 5:32:52
+  // 05:42:09
+
   return (
-    <div>CreateChatbot</div>
+    <div className='flex flex-col items-center justify-center md:flex-row bg-white p-10 rounded-md m-10 md:space-x-10'>
+      <Avatar seed='create-chatebot'/>
+
+      <div>
+        <h1 className="text-xl lg:text-3xl font-semibold">
+          Create
+        </h1>
+        <h2 className="font-light">
+          Create a new chatbot to assist you in your conversations with your customers
+        </h2>
+
+        <form className='flex flex-col md:flex-row gap-2 mt-5'>
+          <Input placeholder='Chatbot Name...' className='max-w-lg' required/>
+          <Button>
+            Create Chatbot
+          </Button>
+        </form>
+      </div>
+    </div>
   )
 }
 
